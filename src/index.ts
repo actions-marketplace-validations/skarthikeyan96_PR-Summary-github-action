f4de7fee573872be9d669a0a1e1a9b3f90562eb2
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-05-06 15:56:39 +0530

    feat: getting the keys via input

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-const message: string = 'Hello, world!';
-console.log(message);
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+const runSummaryAction = () => {
+
+    // if (github.context.eventName !== "pull_request") {
+    //     // The core module on the other hand let's you get
+    //     // inputs or create outputs or control the action flow
+    //     // e.g. by producing a fatal error
+    //     core.setFailed("Can only run on pull requests!");
+    //     return;
+    //   }
+
+
+    const github_token = core.getInput('GITHUB_TOKEN')
+    const openai_key = core.getInput('OPENAI_API_KEY')
+
+    console.log(github_token);
+
+}
+
+runSummaryAction()
+
