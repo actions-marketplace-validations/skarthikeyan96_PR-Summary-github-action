a9e106c398f8c68a786f3631778b810bd8136ef4
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-05-06 16:13:54 +0530

    fix: getting the current PR details

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import axios from 'axios'
 
-const runSummaryAction = () => {
 
-    // if (github.context.eventName !== "pull_request") {
-    //     // The core module on the other hand let's you get
-    //     // inputs or create outputs or control the action flow
-    //     // e.g. by producing a fatal error
-    //     core.setFailed("Can only run on pull requests!");
-    //     return;
-    //   }
+const runSummaryAction = async () => {
+
+    if (github.context.eventName !== "pull_request") {
+        // The core module on the other hand let's you get
+        // inputs or create outputs or control the action flow
+        // e.g. by producing a fatal error
+        core.setFailed("Can only run on pull requests!");
+        return;
+     }
 
 
     const github_token = core.getInput('GITHUB_TOKEN')
     const openai_key = core.getInput('OPENAI_API_KEY')
 
-    console.log(github_token);
+    // get the pr details
+
+    //  const res = await axios.get(`https://api.github.com/repos/${github.repository}/pulls/${github.event.pull_request.number}`)
+     console.log(github.context.issue)
+
 
 }
 
