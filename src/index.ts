77729f271306765d9ff81c2024f62f967d7eeeb3
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-05-06 16:27:39 +0530

    fix: getting the PR details with github api

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,39 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import axios from 'axios'
-
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Octokit } from "@octokit/rest";
+import axios from "axios";
 
 const runSummaryAction = async () => {
+  if (github.context.eventName !== "pull_request") {
+    // The core module on the other hand let's you get
+    // inputs or create outputs or control the action flow
+    // e.g. by producing a fatal error
+    core.setFailed("Can only run on pull requests!");
+    return;
+  }
 
-    if (github.context.eventName !== "pull_request") {
-        // The core module on the other hand let's you get
-        // inputs or create outputs or control the action flow
-        // e.g. by producing a fatal error
-        core.setFailed("Can only run on pull requests!");
-        return;
-     }
+  const github_token = core.getInput("GITHUB_TOKEN");
+  const openai_key = core.getInput("OPENAI_API_KEY");
 
+  const octokit = new Octokit({
+    auth: github_token,
+  });
 
-    const github_token = core.getInput('GITHUB_TOKEN')
-    const openai_key = core.getInput('OPENAI_API_KEY')
+  // get the pr details
 
-    // get the pr details
+  const { repo, owner, number } = github.context.issue;
+  const response = octokit.request(
+    "GET /repos/{owner}/{repo}/pulls/{pull_number}",
+    {
+      owner,
+      repo,
+      pull_number: number,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+  console.log(response);
+};
 
-    //  const res = await axios.get(`https://api.github.com/repos/${github.repository}/pulls/${github.event.pull_request.number}`)
-     console.log(github.context.issue)
-
-
-}
-
-runSummaryAction()
-
+runSummaryAction();
